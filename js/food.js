5e8b75a99864045e735914cd8075dae606f5cb85
Mrzhou-jh <211535176@qq.com>
2020-02-16 16:02:13 +0800

    我的贪吃蛇游戏

--- js/food.js
+++ js/food.js
@@ -9,29 +9,22 @@
     this.higth = options.higth || 25;
     this.color = options.color || "url(img/food.png)";
   }
-
   Food.prototype.produceFood = function(map, snakeList) {
     foodEffectively = false;
     var food_div = document.createElement("div");
-    this.x = Tools.getRandom(0, map.offsetWidth / this.width - 1);
-    this.y = Tools.getRandom(0, map.offsetHeight / this.higth - 1);
+    getRandomX_Y(map, this);
     while (!foodEffectively) {
       foodEffectively = true;
       for (var i = snakeList.length - 1; i > 0; i--) {
-        console.log("检查食物");
-
         //遍历蛇节
         //将蛇节对象与产生的食物对象对比
         if (snakeList[i].x == this.x && snakeList[i].y == this.y) {
           //当有蛇节与食物重合时删除该食物重新产生食物
-          console.log("产生了作废食物");
-          this.x = Tools.getRandom(0, map.offsetWidth / this.width - 1);
-          this.y = Tools.getRandom(0, map.offsetHeight / this.higth - 1);
+          getRandomX_Y(map, this);
           foodEffectively = false;
         }
       }
     }
-
     food_div.style.width = this.width + "px";
     food_div.style.height = this.higth + "px";
     food_div.style.left = this.x * this.width + "px";
@@ -48,6 +41,12 @@
       foodList.pop();
     }
   };
-
+  function getRandomX_Y(map, o) {
+    var maxWidth = map.offsetWidth / o.width - 1;
+    var maxHight = map.offsetHeight / o.higth - 1;
+    var min = 0;
+    o.x = Math.floor(Math.random() * (maxWidth - min + 1) + min);
+    o.y = Math.floor(Math.random() * (maxHight - min + 1) + min);
+  }
   window.Food = Food;
 })();
--- js/game.js
+++ js/game.js
@@ -8,9 +8,13 @@
   var box = document.getElementById("box");
   var difficultyOptions = document.getElementById("difficultyOptions");
   var eatenFood = null; //新吃到的食物对象
-  var pause = null; //游戏状态是否暂停
+  var pause = true; //游戏状态是否暂停
   var gameInterval = null; //游戏循环定时器对象
   var count = 0; //吃到的食物计数
+  var max_x = map.offsetWidth / snake.width,
+    max_y = map.offsetHeight / snake.height,
+    min_x = -1,
+    min_y = -1;
   /* ============首先在map上绘制基本图形================ */
   food.produceFood(map, snake.snakeList); //绘制产生的食物
   snake.produceSnake(map); //绘制蛇
@@ -22,36 +26,13 @@
   var operationBoxList = box.getElementsByTagName("span");
   for (var i = 0; i < operationBoxList.length; i++) {
     operationBoxList[i].onclick = function() {
-      if (pause == true || pause == null) {
+      if (pause == true) {
         return;
       }
-      if (this.innerText === "下" && snake.direction != "t") {
-        //按下键盘下并且蛇正在运行方向不是上
-        snake.direction = "b";
-      } else if (this.innerText === "左" && snake.direction != "r") {
-        //左
-        snake.direction = "l";
-      } else if (this.innerText === "右" && snake.direction != "l") {
-        //右
-        snake.direction = "r";
-      } else if (this.innerText === "上" && snake.direction != "b") {
-        //上
-        snake.direction = "t";
-      } else if (this.innerText === "暂停") {
-        //上
-        console.log("点击了暂停");
-
-        if (!pause) {
-          pause = true;
-          clearInterval(gameInterval);
-          console.log("暂停游戏");
-          start.style.display = "block";
-        }
-      }
+      perationDirection(this.innerText);
     };
   }
-  //设置默认选择难度1
-  sapnList[0].style.background = "red";
+  sapnList[0].style.background = "red"; //设置默认选择难度1
   //遍历难度列表sapnList
   for (let i = 0; i < sapnList.length; i++) {
     //为列表每一项设置点击事件
@@ -66,34 +47,44 @@
       sapnList[i].style.background = "red";
     };
   }
+
   /* ===============监听开始按钮单击事件==================== */
   //单击开始按钮开始游戏
   start.onclick = function(params) {
-    pause = false; //把停止状态设置为false为不停止
-    start.style.display = "none"; //设置开始按钮不可见
     startGame(); //开始游戏
   };
+
   /* ===============开始游戏函数==================== */
   function startGame() {
+    pause = false; //把停止状态设置为false为不停止
+    start.style.display = "none"; //设置开始按钮不可见
     gameInterval = setInterval(
       updatePicture,
       settings.speed / settings.difficulty
     );
+  }
+
+  /* ===============暂停游戏函数==================== */
+  function pauseGame() {
+    pause = true;
+    clearInterval(gameInterval);
+    console.log("暂停游戏");
+    start.style.display = "block";
   }
   //更新画面函数
   function updatePicture() {
     snake.move();
     snake.produceSnake(map);
     var list = snake.snakeList;
-
     if (
-      -1 >= list[0].x ||
-      list[0].x >= map.offsetWidth / snake.width ||
-      -1 >= list[0].y ||
-      list[0].y >= map.offsetHeight / snake.height
+      min_x >= list[0].x ||
+      list[0].x >= max_x ||
+      min_y >= list[0].y ||
+      list[0].y >= max_y
     ) {
-      clearInterval(gameInterval);
+      pauseGame();
       eatenFood = null;
+      count = 0;
       snake.snakeList = [
         { x: 3, y: 2, imgs: "url(img/tou.png)" },
         { x: 2, y: 2, imgs: "url(img/sheng.png)" },
@@ -105,58 +96,60 @@
       score.innerText = 0;
       snake.produceSnake(map);
       if (confirm("游戏结束，点击确定重新开始")) {
-        pause = false;
         startGame();
-      } else {
-        pause = true;
-        start.style.display = "block";
       }
-    }
-    /*判断是否有新吃到的食物有的话就将食物添加到snake.snakeList蛇身数组对象再清空eatenFood对象
-     */
-    if (eatenFood != null) {
-      snake.snakeList.push(eatenFood); //
-      score.innerText = ++count * settings.difficulty;
-      eatenFood = null;
-    }
-    /* 判断当食物的位置与蛇头的位置相同时
-    删除当前食物
-    保存当前蛇身最后一节的位置存在eatenFood吃到的食物对象中
-    每吃到一个食物就在蛇尾增加一节即新的蛇尾
-    */
-
-    if (food.x == list[0].x && food.y == list[0].y) {
-      food.delFood(); //删除食物对象
-      var ophiurid = list[list.length - 1]; //取得当前蛇尾位置对象
-      eatenFood = { x: ophiurid.x, y: ophiurid.y, imgs: ophiurid.imgs }; //取得当前蛇尾位置对象保存到新吃到的食物对象。
-      food.produceFood(map, snake.snakeList); //产生新的食物
+    } else {
+      /*判断是否有新吃到的食物有的话就将食物添加到snake.snakeList蛇身数组对象再清空eatenFood对象
+       */
+      if (eatenFood != null) {
+        list.push(eatenFood);
+        score.innerText = ++count * settings.difficulty;
+        eatenFood = null;
+      }
+      /* 判断当食物的位置与蛇头的位置相同时,删除当前食物,保存当前蛇身最后一节的位置存在eatenFood吃到的食物对象中,每吃到一个食物就在蛇尾增加一节即新的蛇尾
+       */
+      if (food.x == list[0].x && food.y == list[0].y) {
+        food.delFood(); //删除食物对象
+        var ophiurid = list[list.length - 1]; //取得当前蛇尾位置对象
+        eatenFood = { x: ophiurid.x, y: ophiurid.y, imgs: ophiurid.imgs }; //取得当前蛇尾位置对象保存到新吃到的食物对象。
+        food.produceFood(map, list); //产生新的食物
+      }
     }
   }
+
   /* ==============设置监听键盘按下操作事件改编蛇的方向==================== */
   document.onkeydown = function(params) {
-    if (params.keyCode === 40 && snake.direction != "t") {
-      //按下键盘下并且蛇正在运行方向不是上
-      snake.direction = "b";
-    } else if (params.keyCode === 37 && snake.direction != "r") {
-      //左
-      snake.direction = "l";
-    } else if (params.keyCode === 39 && snake.direction != "l") {
-      //右
-      snake.direction = "r";
-    } else if (params.keyCode === 38 && snake.direction != "b") {
-      //上
-      snake.direction = "t";
-    } else if (params.keyCode === 32) {
-      //空格
-      pause = pause == false ? true : false;
-      if (pause) {
-        clearInterval(gameInterval);
-        console.log("暂停游戏");
-        start.style.display = "block";
-      } else {
-        startGame();
-        start.style.display = "none";
-      }
-    }
+    perationDirection(params.keyCode);
   };
+  /* ==============运行方向操作函数==================== */
+  function perationDirection(params) {
+    this.params = params.toString();
+    var movingDirection = snake.direction,
+      negativeDirection = "",
+      newDirection = "";
+    if (this.params === "40" || this.params === "下") {
+      //按下键盘下并且蛇正在运行方向不是上
+      newDirection = "b";
+      negativeDirection = "t";
+    } else if (this.params === "37" || this.params === "左") {
+      //左
+      newDirection = "l";
+      negativeDirection = "r";
+    } else if (this.params === "39" || this.params === "右") {
+      //右
+      newDirection = "r";
+      negativeDirection = "l";
+    } else if (this.params === "38" || this.params === "上") {
+      //上
+      newDirection = "t";
+      negativeDirection = "b";
+    } else if (this.params === "32" || this.params === "暂停") {
+      //空格
+      pause ? startGame() : pauseGame();
+      return;
+    }
+    movingDirection !== negativeDirection
+      ? (snake.direction = newDirection)
+      : (snake.direction = negativeDirection);
+  }
 })();
